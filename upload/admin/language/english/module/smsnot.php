597f1cecdde2817ef7bfaf3cbd8a4850ef3a0c24
SvM <svmidi@gmail.com>
2015-11-05 16:40:24 +0300

    fix language file, and create english file

---
+++ upload/admin/language/english/module/smsnot.php
@@ -0,0 +1,30 @@
+<?php
+$_['heading_title']         = 'Smsnot for SMS.RU';
+$_['button_save']           = 'Сохранить';
+$_['button_cancel']         = 'Отмена';
+$_['button_test']           = 'Отправить тестовое сообщение';
+$_['tab_sending']           = 'Рассылка';
+$_['tab_notice']            = 'Уведомления';
+$_['tab_gate']              = 'Настройки шлюза';
+
+$_['entry_to']               = 'Кому:';
+$_['entry_sender']           = 'Отправитель:';
+$_['entry_message']          = 'Текст сообщения:';
+$_['entry_enabled']          = 'Включить:';
+$_['entry_message_template'] = 'Шаблон сообщения:';
+$_['entry_api_key']          = 'API key:';
+$_['entry_phone']            = 'Номер владельца магазина:';
+$_['entry_balance']          = 'Баланс:';
+
+$_['text_enable']           = 'Включено';
+$_['text_disable']          = 'Выключено';
+$_['text_description']      = 'SMS-уведомления';
+$_['text_newsletter']       = 'Подписаным на новости';
+$_['text_all']              = 'Всем';
+$_['text_all_group']        = 'Всем из группы';
+$_['text_newsletter_group'] = 'Всем из группы подписанным на новости';
+
+$_['text_new_order']        = 'Новый заказ (покупателю)';
+$_['text_order_change']     = 'Изменён статус заказа (покупателю)';
+$_['text_owner']            = 'Уведомление владельца о новом заказе';
+?>
