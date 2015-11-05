54e58bff555a42be42c841ea947b9edec0abe448
SvM <svmidi@gmail.com>
2015-11-05 12:39:34 +0300

    create catalogs

---
+++ upload/admin/controller/module/smsnot.php
@@ -0,0 +1,147 @@
+<?php
+class ControllerModuleSmsnot extends Controller {
+	private $data = array();
+
+	public function index() { 
+
+		$this->load->language('module/smsnot');
+		
+		$this->load->model('module/smsnot');
+		$this->load->model('setting/store');
+		$this->load->model('localisation/language');
+		$this->load->model('setting/setting');
+		
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		if(!isset($this->request->get['store_id'])) {
+		   $this->request->get['store_id'] = 0; 
+		}
+	
+		$store = $this->getCurrentStore($this->request->get['store_id']);
+		
+		if (($this->request->server['REQUEST_METHOD'] == 'POST')) { 	
+			if (!$this->user->hasPermission('modify', 'module/smsnot')) {
+				$this->error['warning'] = $this->language->get('error_permission');
+			}
+
+			if (!$this->user->hasPermission('modify', 'module/smsnot')) {
+				$this->session->data['error'] = 'You do not have permissions to edit this module!';	
+			} else {
+				$this->model_setting_setting->editSetting('Smsnot', $this->request->post, $this->request->post['store_id']);
+				$this->session->data['success'] = $this->language->get('text_success');	
+			}
+			$this->response->redirect(HTTP_SERVER.'index.php?route=module/smsnot&store_id='.$this->request->post['store_id'] . '&token=' . $this->session->data['token']);
+		}
+
+		if (isset($this->session->data['success'])) {
+			$this->data['success'] = $this->session->data['success'];
+			unset($this->session->data['success']);
+		} else {
+			$this->data['success'] = '';
+		}
+
+		if (isset($this->error['warning'])) {
+			$this->data['error_warning'] = $this->error['warning'];
+		} else {
+			$this->data['error_warning'] = '';
+		}
+
+		$this->load->model('localisation/order_status');
+		$this->data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
+
+		$this->data['breadcrumbs']   = array();
+		$this->data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+		);
+		$this->data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_module'),
+			'href' => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'),
+		);
+		$this->data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('module/smsnot', 'token=' . $this->session->data['token'], 'SSL'),
+		);
+		
+		$this->data['heading_title'] = $this->language->get('heading_title');
+		$this->data['button_save'] = $this->language->get('save_changes');
+		$this->data['button_cancel'] = $this->language->get('button_cancel');
+		$this->data['button_test'] = $this->language->get('button_test');
+		$this->data['text_sending'] = $this->language->get('text_sending');
+		$this->data['text_notice'] = $this->language->get('text_notice');
+		$this->data['text_gate'] = $this->language->get('text_gate');
+
+		$this->data['text_to'] = $this->language->get('text_to');
+		$this->data['text_sender'] = $this->language->get('text_sender');
+		$this->data['text_message'] = $this->language->get('text_message');
+		$this->data['text_enabled'] = $this->language->get('text_enabled');
+		$this->data['text_message_template'] = $this->language->get('text_message_template');
+		$this->data['text_api_key'] = $this->language->get('text_api_key');
+		$this->data['text_phone'] = $this->language->get('text_phone');
+		$this->data['text_balance'] = $this->language->get('text_balance');
+		$this->data['text_newsletter'] = $this->language->get('text_newsletter');
+		$this->data['text_all'] = $this->language->get('text_all');
+		$this->data['text_all_group'] = $this->language->get('text_all_group');
+		$this->data['text_newsletter_group'] = $this->language->get('text_newsletter_group');
+		$this->data['text_new_order'] = $this->language->get('text_new_order');
+		$this->data['text_order_change'] = $this->language->get('text_order_change');
+		$this->data['text_owner'] = $this->language->get('text_owner');
+
+		$this->data['stores'] = array_merge(array(0 => array('store_id' => '0', 'name' => $this->config->get('config_name') . ' ' . $this->data['text_default'], 'url' => HTTP_SERVER, 'ssl' => HTTPS_SERVER)), $this->model_setting_store->getStores());
+
+		$this->data['error_warning']  = '';  
+		$this->data['languages']      = $this->model_localisation_language->getLanguages();
+		$this->data['store']          = $store;
+		$this->data['action']         = $this->url->link('module/smsnot', 'token=' . $this->session->data['token'], 'SSL');
+		$this->data['cancel']         = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
+		$this->data['data']           = $this->model_setting_setting->getSetting('SMSBump', $store['store_id']);
+		
+		if(strcmp(VERSION,"2.1.0.1") < 0) {
+			$this->load->model('sale/customer_group');
+			$this->data['customer_groups'] = $this->model_sale_customer_group->getCustomerGroups(0);
+		} else {
+			$this->load->model('customer/customer_group');
+			$this->data['customer_groups'] = $this->model_customer_customer_group->getCustomerGroups(0);
+		}
+	
+
+		$this->data['header']  		= $this->load->controller('common/header');
+		$this->data['column_left']	= $this->load->controller('common/column_left');
+		$this->data['footer']		= $this->load->controller('common/footer');
+		$this->response->setOutput($this->load->view('module/smsnot.tpl', $this->data));
+	}
+
+
+	public function testSend() {
+		
+	}
+	
+	public function install() {
+		$this->load->model('module/smsnot');
+		$this->model_module_smsnot->install();
+		$this->load->model('extension/event');
+		$this->model_extension_event->addEvent('smsnot', 'pre.order.history.add', 'module/smsnot/onCheckout');
+		$this->model_extension_event->addEvent('smsnot', 'post.order.history.add', 'module/smsnot/onHistoryChange');
+	}
+
+	
+	public function uninstall() {
+		$this->load->model('setting/setting');
+		
+		$this->load->model('setting/store');
+		$this->model_setting_setting->deleteSetting('smsnot_module',0);
+		$stores=$this->model_setting_store->getStores();
+		foreach ($stores as $store) {
+			$this->model_setting_setting->deleteSetting('smsnot_module', $store['store_id']);
+		}
+		$this->load->model('module/smsnot');
+		$this->model_module_smsnot->uninstall();
+		$this->load->model('extension/event');
+		$this->model_extension_event->deleteEvent('smsnot');
+	}
+	
+	public function send() {
+		$this->response->setOutput(json_encode($json));	
+	}
+}
+?>
---
+++ upload/admin/language/russian/smsnot.php
@@ -0,0 +1,28 @@
+<?php
+$_['heading_title']         = 'Smsnot for SMS.RU';
+$_['button_save']           = 'Сохранить';
+$_['button_cancel']         = 'Отмена';
+$_['button_test']             = 'Отправить тестовое сообщение';
+$_['text_sending']          = 'Рассылка';
+$_['text_notice']           = 'Уведомления';
+$_['text_gate']             = 'Настройки шлюза';
+
+$_['text_to']               = 'Кому:';
+$_['text_sender']           = 'Отправитель:';
+$_['text_message']          = 'Текст сообщения:';
+$_['text_enabled']          = 'Включить:';
+$_['text_enable']           = 'Включено';
+$_['text_disabled']         = 'Выключено';
+
+$_['text_message_template'] = 'Шаблон сообщения:';
+$_['text_api_key']          = 'API key:';
+$_['text_phone']            = 'Номер владельца магазина:';
+$_['text_balance']          = 'Баланс:';
+$_['text_newsletter']       = 'Подписаным на новости';
+$_['text_all']              = 'Всем';
+$_['text_all_group']        = 'Всем из группы';
+$_['text_newsletter_group'] = 'Всем из группы подписанным на новости';
+$_['text_new_order']        = 'Новый заказ (покупателю)';
+$_['text_order_change']     = 'Изменён статус заказа (покупателю)';
+$_['text_owner']            = 'Уведомление владельца о новом заказе';
+?>
