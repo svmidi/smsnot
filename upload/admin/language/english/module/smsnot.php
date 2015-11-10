448f173433b614855de4b26c268c51cbf1177cff
SvM <svmidi@gmail.com>
2015-11-10 15:22:38 +0300

    fix english translate

--- upload/admin/language/english/module/smsnot.php
+++ upload/admin/language/english/module/smsnot.php
@@ -1,30 +1,38 @@
 <?php
-$_['heading_title']         = 'Smsnot for SMS.RU';
-$_['button_save']           = 'Сохранить';
-$_['button_cancel']         = 'Отмена';
-$_['button_test']           = 'Отправить тестовое сообщение';
-$_['tab_sending']           = 'Рассылка';
-$_['tab_notice']            = 'Уведомления';
-$_['tab_gate']              = 'Настройки шлюза';
+$_['heading_title']          = 'Smsnot for SMS.RU';
+$_['button_save']            = 'Save';
+$_['button_cancel']          = 'Cancel';
+$_['button_test']            = 'Send test message';
+$_['button_send']            = 'Send';
+$_['button_refer']           = 'Create account';
 
-$_['entry_to']               = 'Кому:';
-$_['entry_sender']           = 'Отправитель:';
-$_['entry_message']          = 'Текст сообщения:';
-$_['entry_enabled']          = 'Включить:';
-$_['entry_message_template'] = 'Шаблон сообщения:';
+$_['tab_sending']            = 'Рассылка';
+$_['tab_notice']             = 'Notify';
+$_['tab_gate']               = 'Settings';
+
+$_['entry_to']               = 'To:';
+$_['entry_sender']           = 'From:';
+$_['entry_message']          = 'Text:';
+$_['entry_enabled']          = 'Enable:';
+$_['entry_message_template'] = 'Message template:';
 $_['entry_api_key']          = 'API key:';
-$_['entry_phone']            = 'Номер владельца магазина:';
-$_['entry_balance']          = 'Баланс:';
+$_['entry_phone']            = 'Phone owner:';
+$_['entry_balance']          = 'Balance:';
+$_['entry_characters']       = 'Characters:';
 
-$_['text_enable']           = 'Включено';
-$_['text_disable']          = 'Выключено';
-$_['text_description']      = 'SMS-уведомления';
-$_['text_newsletter']       = 'Подписаным на новости';
-$_['text_all']              = 'Всем';
-$_['text_all_group']        = 'Всем из группы';
-$_['text_newsletter_group'] = 'Всем из группы подписанным на новости';
-
-$_['text_new_order']        = 'Новый заказ (покупателю)';
-$_['text_order_change']     = 'Изменён статус заказа (покупателю)';
-$_['text_owner']            = 'Уведомление владельца о новом заказе';
+$_['text_enable']            = 'enabled';
+$_['text_disable']           = 'disabled';
+$_['text_description']       = 'SMS-message';
+$_['text_newsletter']        = 'Subscribe to news';
+$_['text_all']               = 'All';
+$_['text_all_group']         = 'All of the group';
+$_['text_newsletter_group']  = 'All subscribe to news of the group ';
+$_['text_module']            = 'Smsnot';
+$_['text_refresh']           = 'Refresh';
+$_['text_money_add']         = 'Fill up balance';
+$_['text_new_order']         = 'New order (customer)';
+$_['text_order_change']      = 'Changed order status (customer)';
+$_['text_owner']             = 'Notification of the co-owner of the new order';
+$_['text_send_success']      = 'Message sent';
+$_['text_send_error']        = 'Error sending';
 ?>
