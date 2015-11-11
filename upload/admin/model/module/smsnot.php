6323a30301b8099c289b9355e7a115e75a67c296
SvM <svmidi@gmail.com>
2015-11-11 17:11:46 +0300

    remove old methods

--- upload/admin/model/module/smsnot.php
+++ upload/admin/model/module/smsnot.php
@@ -1,26 +1,22 @@
 <?php 
 class ModelModuleSmsnot extends Model {
-
+	
 	public function install() {
 	// Install Code
-	} 
+	}
 
 	public function uninstall() {
 	// Uninstall Code
 	}
 
 	public function getTotalCustomers($data = array()) {
-		$sql = "SELECT COUNT(*) AS total FROM " . DB_PREFIX . "customer";
+		$sql = "SELECT COUNT(*) AS `total` FROM `" . DB_PREFIX . "customer`";
 		$implode = array();
 		if (isset($data['filter_newsletter']) && !is_null($data['filter_newsletter'])) {
-			$implode[] = "newsletter = '" . (int)$data['filter_newsletter'] . "'";
+			$implode[] = "`newsletter` = '" . (int)$data['filter_newsletter'] . "'";
 		}
-		if (!empty($data['filter_group'])) {
-			$implode[] = "customer_group_id = '" . (int)$data['filter_customer_group_id'] . "'";
-		}
-		if (!empty($data['filter_group_news'])) {
-			$implode[] = "customer_group_id = '" . (int)$data['filter_customer_group_id'] . "'";
-			$implode[] = "newsletter = '" . (int)$data['filter_newsletter'] . "'";
+		if (isset($data['filter_group'])) {
+			$implode[] = "`customer_group_id` = '" . (int)$data['filter_group'] . "'";
 		}
 		if ($implode) {
 			$sql .= " WHERE " . implode(" AND ", $implode);
@@ -29,5 +25,20 @@ class ModelModuleSmsnot extends Model {
 		return $query->row['total'];
 	}
 
+	public function getPhones($data = array()) {
+		$sql = "SELECT `firstname`, `lastname`, `telephone` FROM `" . DB_PREFIX . "customer`";
+		$implode = array();
+		if (isset($data['filter_newsletter']) && !is_null($data['filter_newsletter'])) {
+			$implode[] = "`newsletter` = '" . (int)$data['filter_newsletter'] . "'";
+		}
+		if (isset($data['filter_group'])) {
+			$implode[] = "`customer_group_id` = '" . (int)$data['filter_group'] . "'";
+		}
+		if ($implode) {
+			$sql .= " WHERE " . implode(" AND ", $implode);
+		}
+		$query = $this->db->query($sql);
+		return $query->rows;
+	}
 }
 ?>
